6ac9e231f8dbe41e5dba549039746c1badbae752
Edvaldo Veríssimo <edvaldoverissimo15@gmail.com>
2025-09-02 13:33:14 -0300

    pre case

--- src/Conta.ts
+++ src/Conta.ts
@@ -1,9 +1,11 @@
-export class conta {
+//Definição dos atributos da minha classe conta
+export class Conta {
   private _numero: number;
   private _agencia: number;
   private _tipo: number;
   private _titular: string;
   private _saldo: number;
+  //Me esqueci (pesquisar o que é o contructor novamente)
 
   constructor(
     numero: number,
@@ -18,6 +20,7 @@ export class conta {
     this._titular = titular;
     this._saldo = saldo;
   }
+  //Getters e setters, servem pra visualizar(get) modificar(set) os atributos
 
   public get numero() {
     return this._numero;
@@ -57,5 +60,31 @@ export class conta {
 
   public set saldo(saldo: number) {
     this._saldo = saldo;
+  }
+  //Metodos de ação
+  //sacar
+  public sacar(valor: number): boolean {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return true;
+    }
+    console.log("\n Saldo insuficiente para o Saque!");
+    return false;
+  }
+  //deposi
+  public depositar(valor: number): void {
+    this._saldo += valor;
+    console.log("\nDepósito realizado com sucesso!");
+  }
+
+  public visualizar(): void {
+    console.log("\n*************************************");
+    console.log("        Dados da Conta");
+    console.log("*************************************");
+    console.log(`Número da Conta: ${this._numero}`);
+    console.log(`Agência: ${this._agencia}`);
+    console.log(`Tipo da Conta: ${this._tipo}`);
+    console.log(`Titular: ${this._titular}`);
+    console.log(`Saldo: R$ ${this._saldo.toFixed(2)}`);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-const nome: string = "Edval";
-console.log(`Olá, ${nome}!`);
+import { Conta } from "./Conta.js";
+import * as readline from "readline-sync";
+
+//declarar que digite é a variavel que chama entreda
+const digite = require("readline-sync");
+
+//
+const conta1 = new Conta(1, 123, 1, "ECOLEITON RASTA", 2400);
+const conta2 = new Conta(2, 124, 1, "Clovis bucetaldo da silva", 30000);
+
+//para o case
+let entrada = 0;
+
+do {
+    console.clear;
+    console.log(`         *********************************
+                Messi Bank — driblando burocracias, marcando conquistas.
+                          *********************************
+                    1 - Criar Conta
+                    2 - Çostar todas as Contas
+                    3 - Buscar Conta por Numero
+                    4 - Atualizar Dados de conta
+                    5 - Apagar Conta
+                    6 - Sacar  Conta
+                    8 - Transferir valores entre Contas
+                    9 - sair`);
+    
+    entrada = digite.questionInt("Digite a opcao desejada : ");
+
+    
+                
+
+}
